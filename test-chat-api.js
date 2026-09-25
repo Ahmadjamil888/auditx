7b631a6ad1bbd964d1e0662f80f24dd3b220bb1b
ADMIN <ahmadjamildhami@gmail.com>
2026-09-26 00:10:30 +0500

    lklk

---
+++ test-chat-api.js
@@ -0,0 +1,9 @@
+// Test script for AuditX Chat API
+// Note: This API requires authentication. For testing, use the actual UI at http://localhost:8080
+console.log('The chat API requires authentication with a Bearer token.');
+console.log('To test the chat functionality:');
+console.log('1. Open http://localhost:8080 in your browser');
+console.log('2. Sign in or sign up for an account');
+console.log('3. Navigate to the chat interface');
+console.log('4. Send a test message');
+console.log('\nDev server is running at http://localhost:8080');
